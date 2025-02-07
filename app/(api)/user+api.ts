202d5c8a069a2a31fc0ba2cd8f8ce8e530580393
Baas Zunnaiyyer <baaszunnaiyyer@gmail.com>
2025-02-07 18:39:54 +0500

    Starter Template for React Native Expo with Clerk

--- app/(api)/user+api.ts
+++ app/(api)/user+api.ts
@@ -1,3 +1,34 @@
+import { data } from '@/constants';
 import { neon } from '@neondatabase/serverless';
 
-const sql = neon(process.env.DATABASE_URL);
+
+export async function Post(request:Request) {
+    const sql = neon(`${process.env.DATABASE_URL}`);
+    const {name, email, clerkId} = await request.json();
+
+    if(!name || !email || !clerkId){
+        return Response.json({
+            data: {error :"Missing Required Fields"},
+            init: {status: 400 }
+        })
+    }
+
+    const response = await sql`
+        INSERT INTO users(
+            name,
+            email,
+            clerk_id,
+        )
+        VALUES(
+            ${name},
+            ${email},
+            ${clerkId}
+        )
+    `
+
+    try {
+        
+    } catch (err) {
+        
+    }
+}
